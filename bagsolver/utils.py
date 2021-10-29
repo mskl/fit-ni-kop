147ee3c2baa178c65f80d411147881385686edb5
Matyáš Skalický <matyas.skalicky@rossum.ai>
2021-10-29 10:05:36 +0200

    task 2 coding seems to be finished

--- bagsolver/utils.py
+++ bagsolver/utils.py
@@ -1,3 +1,9 @@
 def load_bag_data(x_file, y_file):
     with open(x_file) as x, open(y_file) as y:
         return tuple(zip(x.readlines(), y.readlines()))
+
+
+def parse_solution(bag_sol):
+    parsed = [int(v) for v in bag_sol.strip().split(" ")]
+    iid, count, target_cost, *target_items = parsed
+    return iid, count, target_cost, target_items
--- tests/test_bag.py
+++ tests/test_bag.py
@@ -1,7 +1,6 @@
-import numpy as np
 import pytest
 from bagsolver.bag import Bag
-from bagsolver.utils import load_bag_data
+from bagsolver.utils import load_bag_data, parse_solution
 
 
 @pytest.mark.parametrize(
@@ -18,62 +17,47 @@ def test_bag_load(rawline, expected):
     assert bag.capacity == expected.capacity
 
 
-def get_dataset():
-    return load_bag_data("data/NK/NK10_inst.dat", "data/NK/NK10_sol.dat")[:10]
+def get_dataset(subsample=100):
+    return load_bag_data("data/NK/NK10_inst.dat", "data/NK/NK10_sol.dat")[:subsample]
 
 
 @pytest.mark.parametrize("bag_def, bag_sol", get_dataset())
 @pytest.mark.parametrize("optimizations", [None, {"residuals"}, {"weight"}, {"weight", "residuals"}])
 def test_bag_solve(bag_def, bag_sol, optimizations):
-    bag = Bag.from_line(bag_def)
-    res = bag.solve_bb(optimizations=optimizations)
+    res = Bag.from_line(bag_def).solve_bb(optimizations=optimizations)
 
-    parsed = [int(v) for v in bag_sol.strip().split(" ")]
-    iid, count, target_cost, *target_items = parsed
-    assert bag.iid == iid
-    assert tuple(int(i) for i in bag.best_solution) == tuple(i for i in target_items)
+    iid, count, target_cost, target_items = parse_solution(bag_sol)
     assert res == target_cost
 
 
 @pytest.mark.parametrize("bag_def, bag_sol", get_dataset())
 @pytest.mark.parametrize("redux", [False, True])
 def test_bag_solve_greedy(bag_def, bag_sol, redux):
-    bag = Bag.from_line(bag_def)
-    res = bag.solve_greedy(redux)
+    res = Bag.from_line(bag_def).solve_greedy(redux)
 
-    parsed = [int(v) for v in bag_sol.strip().split(" ")]
-    iid, count, target_cost, *target_items = parsed
-    assert bag.iid == iid
+    iid, count, target_cost, target_items = parse_solution(bag_sol)
+    assert (res - target_cost) < 1000
 
 
 @pytest.mark.parametrize("bag_def, bag_sol", get_dataset())
 def test_bag_dynamic_cost(bag_def, bag_sol):
-    bag = Bag.from_line(bag_def)
-    res = bag.solve_dynamic_cost()
+    res = Bag.from_line(bag_def).solve_dynamic_cost()
 
-    parsed = [int(v) for v in bag_sol.strip().split(" ")]
-    iid, count, target_cost, *target_items = parsed
+    iid, count, target_cost, target_items = parse_solution(bag_sol)
     assert res == target_cost
-    assert bag.iid == iid
 
 
 @pytest.mark.parametrize("bag_def, bag_sol", get_dataset())
 def test_bag_dynamic_weight(bag_def, bag_sol):
-    bag = Bag.from_line(bag_def)
-    res = bag.solve_dynamic_weight()
+    res = Bag.from_line(bag_def).solve_dynamic_weight()
 
-    parsed = [int(v) for v in bag_sol.strip().split(" ")]
-    iid, count, target_cost, *target_items = parsed
+    iid, count, target_cost, target_items = parse_solution(bag_sol)
     assert res == target_cost
-    assert bag.iid == iid
 
 
 @pytest.mark.parametrize("bag_def, bag_sol", get_dataset())
 def test_bag_ftapas(bag_def, bag_sol):
-    bag = Bag.from_line(bag_def)
-    res = bag.solve_ftapas(.3)
+    res = Bag.from_line(bag_def).solve_ftapas(.3)
 
-    parsed = [int(v) for v in bag_sol.strip().split(" ")]
-    iid, count, target_cost, *target_items = parsed
-    assert res == target_cost
-    assert bag.iid == iid
+    iid, count, target_cost, target_items = parse_solution(bag_sol)
+    assert (res - target_cost) < 100
